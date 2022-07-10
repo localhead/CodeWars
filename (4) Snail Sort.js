7e002bd859e0b9310b51f7a7c1817bda0538dd23
localhead <dodoalive1994@gmail.com>
2022-07-11 02:08:28 +0300

    snail sort

--- (4) Snail Sort.js
+++ (4) Snail Sort.js
@@ -1,1 +1,36 @@
 'use strict';
+/* 
+
+
+Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+
+array = [[1,2,3],
+         [4,5,6],
+         [7,8,9]]
+snail(array) #=> [1,2,3,6,9,8,7,4,5]
+For better understanding, please follow the numbers of the next array consecutively:
+
+array = [[1,2,3],
+         [8,9,4],
+         [7,6,5]]
+snail(array) #=> [1,2,3,4,5,6,7,8,9]
+This image will illustrate things more clearly:
+
+*/
+
+const array0 = [
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9, 10],
+  [11, 12, 13, 14, 15],
+  [16, 17, 18, 19, 20],
+  [21, 22, 23, 24, 25],
+];
+
+const snail = function (array) {
+  let globalCount = 0;
+  // to the right
+  console.log(array[0][0]);
+  console.log(array0.length);
+};
+
+snail(array0);
